76982e5ad10e7c19ae141318e8553b59aed578ae
Ryan Bigg <radarlistener@gmail.com>
2014-02-05 10:30:05 +0000

    move cc details to a variable

--- spec/adyen_spec.rb
+++ spec/adyen_spec.rb
@@ -14,12 +14,20 @@ describe 'Adyen' do
   it 'can authorize a payment' do
     invoice = double(id: 1, amount: 100)
     user = double(id: 1, email: 'test@example.com')
+
+    cc_details = { 
+      :holder_name => "Simon Hopper",
+      :number => '4111111111111111',
+      :cvc => '737',
+      :expiry_month => 6, 
+      :expiry_year => 2016
+    }
+
     response = Adyen::API.authorise_payment(
       invoice.id,
       { :currency => 'GBP', :value => invoice.amount },
       { :reference => user.id, :email => user.email, :ip => '8.8.8.8', :statement => 'invoice number 123456'},
-      { :holder_name => "Simon Hopper", :number => '4111111111111111', :cvc => '737',
-        :expiry_month => 6, :expiry_year => 2016 }
+      cc_details
     )
 
     expect(response.authorized?).to be_true
